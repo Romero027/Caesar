2522a0668d2bae5b42e7abcb79ce3738ead44030
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2020-07-30 11:18:48 -0400

    added tracker gt

---
+++ main_mobile_gt.py
@@ -0,0 +1,178 @@
+from __future__ import absolute_import, division, print_function
+
+'''
+Function: read a video, run detection and send to next hop
+
+Params: video path
+
+Outputs: serialized GRPC packet (defined in network/data_packet)
+            packet meta: a list of {'box':[x0,y0,x1,y1], # in pixel position
+                                    'score': confidence score of det in float,
+                                    'label': name of the object in str}
+'''
+
+import sys
+import os, re
+import numpy as np
+import logging
+from time import time, sleep
+from multiprocessing import Process
+import json
+from threading import Thread
+
+import config.const_mobile as const
+from network.data_reader import DataReader
+from network.data_reader_image import DataReaderIMG
+from network.data_writer import DataWriter
+from network.socket_client import NetClient
+from network.data_packet import DataPkt
+
+
+"""
+Create result folder
+"""
+RES_FOLDER = 'res/{}/'.format(os.path.basename(__file__).split('.py')[0])
+if not os.path.exists(RES_FOLDER):
+    os.makedirs(RES_FOLDER)
+print('Output to {}'.format(RES_FOLDER))
+
+
+"""
+Main function
+"""
+def main(running):
+    reader = DataReaderIMG(img_path="/Users/allenzhu/Desktop/Research/Video/Wildtrack_dataset/Image_subsets/C1",
+                        file_path='',
+                        max_frame_id=const.MAX_FRAME_ID)
+
+    detector = None
+    # if const.OBJ_MODEL == 'mobilenet':
+    #     from mobile.object_detector_tf import TFDetector
+    #     detector = TFDetector(
+    #         graph_path=const.OBJ_MODEL_PATH,
+    #         label_file=const.OBJ_LABEL_FILE,
+    #     )
+    # elif const.OBJ_MODEL == 'mrcnn':
+    #     '''
+    #     mrcnn_path = os.path.join(os.getcwd(), 'mobile/maskrcnn_benchmark')
+    #     print("mask rcnn path", mrcnn_path)
+    #     sys.path.append(mrcnn_path)
+    #     '''
+    #     from mobile.object_detector_mrcnn_torch import MRCNN
+    #     detector = MRCNN(model_path=const.OBJ_MODEL_PATH)
+    # else:
+    #     raise ValueError("Model not implemented!")
+
+    uploader = NetClient(
+        client_name=const.CLIENT_NAME,
+        server_addr=const.SERVER_ADDR,
+        buffer_size=const.QUEUE_SIZE,
+    )
+
+    if const.UPLOAD_DATA:
+        uploader_proc = Process(target=uploader.run)
+        uploader_proc.start()
+
+    data_saver = DataWriter(
+        file_path=RES_FOLDER + '{}.npy'.format(const.CLIENT_NAME)
+    )
+
+    print('Mobile init done!')
+
+    frame_cache = []
+    frame_id_cache = []
+
+    timer1 = time()
+    timer2 = time()
+    count = 0
+    time_gap = float(const.OBJ_BATCH_SIZE) / float(const.UPLOAD_FPS)
+
+    path = "/Users/allenzhu/Desktop/Research/Video/Wildtrack_dataset/annotations_positions/"
+
+    while running[0]:
+        img, frame_id, frame_name, meta = reader.get_data()
+        if not len(img):
+            break
+        
+        # Print frame id and FPS every 20 frames
+        if not frame_id % const.OBJ_BATCH_SIZE:
+            print('frame {}, avg FPS {}'.format(
+                    frame_id,
+                    const.OBJ_BATCH_SIZE / (time()-timer1),
+                )
+            )
+            timer1 = time()
+
+
+        frame_name = re.findall(r'\d+',frame_name.split("/")[-1])[0]
+        if frame_name == "00002000":
+            continue
+        with open(path+ frame_name +".json") as j:
+            data = json.load(j)
+
+        meta = []
+        for d in data:
+            for v in d['views']:
+                if v['viewNum'] != 0 or v['xmax'] == -1:
+                    continue
+
+                x = v['xmin']
+                y = v['ymin']
+                w = v['xmax']
+                h = v['ymax']
+                if x < 0 or y < 0 or w < 0 or h < 0:
+                    continue
+            meta.append({'box': [int(x), int(y),
+                                int(w), int(h)],
+                        'label': "person",
+                        'score': 1.0})
+
+        pkt = DataPkt(
+            img=img,
+            cam_id=const.CLIENT_NAME,
+            frame_id=frame_id,
+            frame_name=frame_name,
+            meta=meta,
+        )
+        sleep(2)
+        print(frame_name)
+
+        
+        # if const.UPLOAD_DATA:
+        #     uploader.send_data(pkt)
+        
+        # if const.SAVE_DATA:
+        #     data_saver.save_data(
+        #         frame_id=frame_id,
+        #         meta=pkt.meta,
+        #     )
+
+        # time_past = time() - timer2
+        # sleep_time = max(0, time_gap - time_past)
+        # sleep(sleep_time)
+        # timer2 = time()
+        
+        # frame_cache = []
+        # frame_id_cache = []
+    #
+    # if const.SAVE_DATA:
+    #     data_saver.save_to_file()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='mobile_debug.log',
+                        format='%(asctime)s %(message)s',
+                        datefmt='%I:%M:%S ',
+                        filemode='w',
+                        level=logging.DEBUG)
+    running = [True]
+    main(running)
+    # th = Thread(target=main, args=(running,))
+    # th.start()
+    # while True:
+    #     try:
+    #         sleep(10)
+    #     except (KeyboardInterrupt, SystemExit):
+    #         running[0] = False
+    #         break
+    # print('done')
